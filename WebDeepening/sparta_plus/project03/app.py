da0cd687a904fc62ab7bc5364b05c9817015070c
eunag63 <77442567+eunag63@users.noreply.github.com>
2021-09-14 17:35:40 +0900

    맛집지도 DB에서 맛집정보 받아오기

--- WebDeepening/sparta_plus/project03/app.py
+++ WebDeepening/sparta_plus/project03/app.py
@@ -15,8 +15,10 @@ def main():
 
 @app.route('/matjip', methods=["GET"])
 def get_matjip():
-    # 맛집 목록을 반환하는 API
-    return jsonify({'result': 'success', 'matjip_list': []})
+    # 데이터베이스에서 맛집 목록을 꺼내와야 한다
+    matjip_list = list(db.matjips.find({}, {'_id': False}))
+    # matjip_list 라는 키 값에 맛집 목록을 담아 클라이언트에게 반환합니다.
+    return jsonify({'result': 'success', 'matjip_list': matjip_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
