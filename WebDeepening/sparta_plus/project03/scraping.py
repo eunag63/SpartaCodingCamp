3e66aabedc190e76bd4560716e7d23595b1923ca
eunag63 <77442567+eunag63@users.noreply.github.com>
2021-09-14 17:10:04 +0900

    맛집지도 DB에 저장

--- WebDeepening/sparta_plus/project03/scraping.py
+++ WebDeepening/sparta_plus/project03/scraping.py
@@ -16,6 +16,15 @@ url = "http://matstar.sbs.co.kr/location.html"
 driver.get(url)
 time.sleep(5)
 
+# 버튼 10번 누르기
+for i in range(10):
+    try:
+        btn_more = driver.find_element_by_css_selector("#foodstar-front-location-curation-more-self > div > button")
+        btn_more.click()
+        time.sleep(5)
+    except NoSuchElementException:
+        break
+
 req = driver.page_source
 driver.quit()
 
@@ -44,5 +53,17 @@ for place in places:
             x = float(response["addresses"][0]["x"])
             y = float(response["addresses"][0]["y"])
             print(title, address, category, show, episode, x, y)
+
+            # DB저장
+            doc = {
+                "title": title,
+                "address": address,
+                "category": category,
+                "show": show,
+                "episode": episode,
+                "mapx": x,
+                "mapy": y}
+            db.matjips.insert_one(doc)
+
         else:
             print(title, "좌표를 찾지 못했습니다")
