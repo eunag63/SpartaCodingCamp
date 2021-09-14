1c4f3a92f1273bf665a5fff73e97bbb063960a27
eunag63 <77442567+eunag63@users.noreply.github.com>
2021-09-14 16:47:49 +0900

    맛집지도 세팅

--- WebDeepening/sparta_plus/project03/app.py
+++ WebDeepening/sparta_plus/project03/app.py
@@ -12,9 +12,11 @@ db = client.dbsparta_plus_week3
 def main():
     return render_template("index.html")
 
-@app.route('/map')
-def test_map():
-    return render_template("prac_map.html")
+
+@app.route('/matjip', methods=["GET"])
+def get_matjip():
+    # 맛집 목록을 반환하는 API
+    return jsonify({'result': 'success', 'matjip_list': []})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
