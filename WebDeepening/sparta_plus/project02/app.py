8f5aa2e46c18321e85db8c7bb1e8bcf7a7fefa77
eunag63 <77442567+eunag63@users.noreply.github.com>
2021-09-14 15:06:40 +0900

    나만의단어장 사전에 없는 단어 기능

--- WebDeepening/sparta_plus/project02/app.py
+++ WebDeepening/sparta_plus/project02/app.py
@@ -13,9 +13,12 @@ db = client.dbsparta_plus_week2
 
 @app.route('/')
 def main():
+    # 메시지 보내주기
+    msg = request.args.get("msg")
+
     # DB에서 저장된 단어 찾아서 HTML에 나타내기
     words = list(db.words.find({}, {"_id": False}))
-    return render_template("index.html", words=words)
+    return render_template("index.html", words=words, msg=msg)
 
 
 @app.route('/detail/<keyword>')
@@ -25,6 +28,8 @@ def detail(keyword):
 
     # API에서 단어 뜻 찾아서 결과 보내기
     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token 7e530070d87fefd64570d05ee1e2db30d67332b4"})
+    if r.status_code != 200:
+        return redirect(url_for("main", msg="단어가 이상합니다!"))
     result = r.json()
     print(result)
 
